5fc45c78212abe1983164fce5ea52fa6f4e3b803
jacobblad <66271471+jacobblad@users.noreply.github.com>
2021-04-09 12:13:51 -0600

    added connection string and packages

--- Controllers/SecretController.cs
+++ Controllers/SecretController.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace FagElGamous.Controllers
-{ 
+{
     [Authorize]
     public class SecretController : Controller
     {
