7f4cc17146d68ab5fe1f13d8a7dff2f20972d52c
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2021-04-10 10:16:52 -0600

    Linq-ed databases and pulled data from it. :)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace FagElGamous.Controllers
 {
@@ -24,6 +25,35 @@ namespace FagElGamous.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+        public IActionResult MultiDatabase()
+        {
+            using (mummiesdbContext db = new mummiesdbContext())
+            {
+                List<Burial> burials = db.Burial.ToList();
+                List<Sample> samples = db.Sample.ToList();
+                List<C14> C14s = db.C14.ToList();
+                List<Cranial> cranials = db.Cranial.ToList();
+
+                var mummyRecord = from b in burials
+                                  join s in samples on b.BurialId equals s.BurialFk into table1
+                                  from s in table1.ToList()
+                                  join C in C14s on b.BurialId equals C.BurialFk into table2
+                                  from C in table2.ToList()
+                                      //join c in cranials on b.BurialId equals c.BurialFk into table3
+                                      //from c in table3.ToList()
+                                  select new ViewModel
+                                  {
+                                      burial = b,
+                                      sample = s,
+                                      c14 = C,
+                                      //cranial = c
+                                  };
+                return View(mummyRecord);
+            }
         }
 
         public IActionResult AllSites(/*int pageNum = 1*/)
