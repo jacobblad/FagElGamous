8096394eda24f33310b16eb3b33d3a321bcbeb2e
benjaminhale <33043151+benjaminhale@users.noreply.github.com>
2021-04-10 11:51:26 -0600

    Added comment for possible filtering management.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,13 +35,13 @@ namespace FagElGamous.Controllers
             {
                 List<Burial> burials = db.Burial.ToList();
                 List<Sample> samples = db.Sample.ToList();
-                List<C14> C14s = db.C14.ToList();
+                List<C14> c14s = db.C14.ToList();
                 List<Cranial> cranials = db.Cranial.ToList();
 
                 var mummyRecord = from b in burials
                                   join s in samples on b.BurialId equals s.BurialFk into table1
                                   from s in table1.ToList()
-                                  join C in C14s on b.BurialId equals C.BurialFk into table2
+                                  join C in c14s on b.BurialId equals C.BurialFk into table2
                                   from C in table2.ToList()
                                       //join c in cranials on b.BurialId equals c.BurialFk into table3
                                       //from c in table3.ToList()
@@ -55,6 +55,33 @@ namespace FagElGamous.Controllers
                 return View(mummyRecord);
             }
         }
+
+
+        /*
+        //This is me trying to figure out how to get possible table data from filtering
+        //Two field matching
+        [HttpPost]
+        public IActionResult MultiDatabase(var fieldName1, var recordValue1, var fieldName2, var recordValue2)
+        //For three field matching, maybe add field and record value 3? Maybe overload the function?
+        {
+            using (mummiesdbContext db = new mummiesdbContext())
+            {
+                List<Burial> burials = db.Burial.ToList();
+                List<Sample> samples = db.Sample.ToList();
+                List<C14> c14s = db.C14.ToList();
+                List<Cranial> cranials = db.Cranial.ToList();
+
+                var mummyRecord = from table in burials
+                                  where fieldName1 equals recordValue1
+                                  and fieldName2 equals recordValue2
+                                  //For three field matching, add values 3?
+                                  select new ViewModel
+                                  {
+                                      filtered = table,
+                                  };
+                return View(mummyRecord);
+            }
+        }*/
 
         public IActionResult AllSites(/*int pageNum = 1*/)
         {
