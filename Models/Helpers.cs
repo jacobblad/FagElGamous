18409427aea8f1d3f51b2b402314f06df94827c3
sagehassell <60795541+sagehassell@users.noreply.github.com>
2021-04-10 19:40:59 -0600

    helper connection string changes

---
+++ Models/Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FagElGamous.Models
+{
+    public class Helpers
+    {
+        public static string GetRDSConnectionString()
+        {
+            var appConfig = ConfigurationManager.AppSettings;
+
+            string dbname = appConfig["mummiesdb"];
+
+            if (string.IsNullOrEmpty(dbname)) return null;
+
+            string username = appConfig["admin"];
+            string password = appConfig["hereisagreatpasswordnobodywillguess"];
+            string hostname = appConfig["rds-mysql-mummies.cnhhfictxlmw.us-east-1.rds.amazonaws.com"];
+            string port = appConfig["3306"];
+
+            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+        }
+    }
+}
